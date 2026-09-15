aca398ba30b8f4e54e43b55a616fcacc3432f8a3
Womsxd <45663319+Womsxd@users.noreply.github.com>
2026-09-15 20:57:50 +0800

    bump version 1.1.1 (liyue/璃月)
    Signed-off-by: Womsxd <45663319+Womsxd@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "dev.woms.mumdroid"
         minSdk = 26
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
         buildConfigField("String", "GIT_HASH", "\"$gitHash\"")
         buildConfigField("Long", "BUILD_TIME", "${System.currentTimeMillis()}L")
         buildConfigField("String", "versionCodeName", "\"Liyue\"")
